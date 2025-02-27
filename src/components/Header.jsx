3757dbb57cdcaadab960d80c907cff9e6878b304
Arthur VIENOT ONGARO <arthurvienot1008@gmail.com>
2025-02-27 17:45:41 +0100

    change logo path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import Nav from "./Nav";
+import logo from "@/assets/react.svg"; // Importation correcte du logo
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4 md:px-10 py-4 gap-4">
         {/* Logo et menu burger */}
         <div className="w-full md:w-auto flex justify-between items-center gap-7">
-          <img src="/src/assets/react.svg" alt="logo" className="h-8 md:h-10" />
+          <img src={logo} alt="logo" className="h-8 md:h-10" /> {/* Utilisation de l'import */}
           <Nav />
         </div>
 
